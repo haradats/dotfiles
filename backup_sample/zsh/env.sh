155dad931e3b988f1aada2e933da76e48f0d197f
Toshiharu Harada <haradats@gmail.com>
2022-12-31 14:13:42 +0900

    added source zsh-syntax-highlighting.zsh for osx (homebrew) and Ubuntu

--- backup_sample/zsh/env.sh
+++ backup_sample/zsh/env.sh
@@ -1,1 +1,4 @@
 export DATABASE_PASSWORD='your_password'
+
+# source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # osx (homebrew)
+# source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # ubuntu
