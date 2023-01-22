278532b30afa69976bcd482d9d6ea71e712c8a19
Toshiharu Harada <haradats@gmail.com>
2023-01-22 12:25:42 +0900

    detect if running WSL

--- backup_sample/zsh/env.sh
+++ backup_sample/zsh/env.sh
@@ -1,5 +1,16 @@
 export DATABASE_PASSWORD='your_password'
+
 export ALTERNATE_EDITOR=vim
 
+if [ ! `uname -r | grep -iq wsl` ]; then
+    # running WSL
+    source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # ubuntu
+
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
+
 # source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # osx (homebrew)
-# source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # ubuntu
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
