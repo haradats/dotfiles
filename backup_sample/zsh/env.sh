1480483b9afe142b03c4270d338ad0e82c9aa0eb
Toshiharu Harada <haradats@gmail.com>
2023-01-22 11:49:54 +0900

    define ALTERNATE_EDITOR

--- backup_sample/zsh/env.sh
+++ backup_sample/zsh/env.sh
@@ -1,4 +1,5 @@
 export DATABASE_PASSWORD='your_password'
+export ALTERNATE_EDITOR=vim
 
 # source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # osx (homebrew)
 # source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # ubuntu
