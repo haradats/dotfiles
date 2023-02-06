96405d95d6995748869bdff10b77b38fd6a9590d
Toshiharu Harada <haradats@gmail.com>
2023-02-06 20:25:14 +0900

    fixed regex

--- backup_sample/zsh/env.sh
+++ backup_sample/zsh/env.sh
@@ -2,18 +2,20 @@ export DATABASE_PASSWORD='your_password'
 export ALTERNATE_EDITOR=vim
 
 local uname=`uname -r`
-local wsl1='*[mM]icrosoft'
-local wsl2='*wsl'
-local mac='[0-9]+.[0-9]+.[0-9]+'
+local wsl1='[ -][mM]icrosoft[ -]'
+local wsl2='[ -](wsl | WSL)[ -]'
+local mac='[0-9]+\.[0-9]+\.[0-9]+'
 
-if [[ "$uname" == $~wsl1 || "$uname" == $~wsl2 ]]; then
+if [[ $uname =~ $wsl1 || $uname =~ $wsl2 ]]; then
     # running WSL
+    echo WSL
     source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # ubuntu
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-elif [[ $uname == $~mac ]]; then
+elif [[ $uname =~ $mac ]]; then
     ### running mac (homebrew)
+    echo mac
     source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
     export NVM_DIR="$HOME/.nvm"
     [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
