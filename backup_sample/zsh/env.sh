aa719768469dcd6648d09b503eb92460d88b4380
Toshiharu Harada <haradats@users.noreply.github.com>
2024-10-06 13:02:16 +0900

    osx syntax highlighting

--- backup_sample/zsh/env.sh
+++ backup_sample/zsh/env.sh
@@ -25,7 +25,7 @@ if [[ $uname =~ $wsl1 || $uname =~ $wsl2 ]]; then
 elif [[ $uname =~ $mac ]]; then
     ### running mac (homebrew)
     echo " (mac)"
-    source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+    source /opt/homebrew/opt/zsh-fast-syntax-highlighting/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.zsh
     export NVM_DIR="$HOME/.nvm"
     [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
     [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
